e7c19e858c4c3ada32664f9f27847ae6d6f55857
Jingyang Fan <jingyangfan@Jingyangs-MacBook-Pro.local>
2020-10-15 13:56:42 -0400

    changes to nav

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,6 +4,7 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
+    <Link to="/index/">Home</Link>
     <Link to="/contact">Contact</Link>
     <Link to="/contact">Gallery</Link>
     <ThemeChanger/>
