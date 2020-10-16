6541df118d1390dee5d5701b4f6ecb62956cf3f9
Jingyang Fan <jingyangfan@Jingyangs-MacBook-Pro.local>
2020-10-15 21:09:08 -0400

    color changes

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,8 +5,8 @@ import ThemeChanger from "../components/themeChanger"
 export default (props) => (
   <nav className="navigation"> 
     <Link to="/">Home</Link>
-    <Link to="/contact">Contact</Link>
     <Link to="/404">Gallery</Link>
+    <Link to="/contact">Contact</Link>
     <ThemeChanger/>
   </nav>
   
